88a708610343c6fe764d01360b367098c9437eef
wendymasese <wendy.masese@student.moringaschool.com>
2024-04-09 18:04:30 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
-}
+    alert('I was clicked!');
+
+
+const input = document.getElementById('button');
+button.addEventListener ('Click',function(){
+    console.log ('Button clicked!');
+});
+
+return true;}
+
+
